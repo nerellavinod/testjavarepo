5c57a32f620365001f48a58c595915d6157f833b
Vinod Nerella <vinod1502506@mastek.com>
2025-12-04 22:13:24 +0530

    ddsa

--- Deployment/src/provider.tf
+++ Deployment/src/provider.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "azurerm" {
+    resource_group_name  = "TempAugRG"
+    storage_account_name = "tempaugstorage"
+    container_name       = "tfstate"
+    key                  = "hmnao.tfstate"
   }
 }
 
